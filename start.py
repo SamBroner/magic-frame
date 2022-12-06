f21a9483c42fd56db40336f51924a2f74f57607c
Sam Broner <sam.broner@gmail.com>
2022-12-06 05:32:59 +0000

    some stuff is figured out

--- start.py
+++ start.py
@@ -6,6 +6,17 @@ import requests
 import urllib
 import urllib.request
 from pathlib import Path
+import asyncio
+
+import pvporcupine
+import pvcheetah
+
+import pyaudio
+import websockets
+import asyncio
+import base64
+import json
+import struct
 
 from IT8951.display import AutoEPDDisplay
 from IT8951 import constants
@@ -33,39 +44,151 @@ default_display(display)
 
 test_dalle = False
 
-def run_display():
+# def run_display():
 
-    # Create Dalle
-    dalle = Dalle2(dalle_key)
+#     # Create Dalle
+#     dalle = Dalle2(dalle_key)
 
-    # Create Voice Manager
-    vm = VoiceManager(pico_access_key=pico_key, keywords=["jarvis"], input_device_index=-1)
-    time.sleep(10)
-    default_display(display)
+#     # Create Voice Manager
+#     vm = VoiceManager(pico_access_key=pico_key, keywords=["jarvis"], input_device_index=-1)
+#     time.sleep(10)
+#     default_display(display)
 
-    transcript = vm.run(display)
-    print(transcript)
+#     transcript = vm.run(display)
+#     print(transcript)
 
-    if (test_dalle):
-        dalle.generate_and_download(transcript, "./imgs/tmp")
-        temp_dir = "./imgs/tmp"
-        targ_dir = "./imgs"
-        i = 0
-        for file_name in os.listdir(temp_dir):
-            os.rename(os.path.join(temp_dir,file_name), os.path.join(targ_dir,transcript + "-" + str(i) + ".webp"))
-            i += 1
+#     if (test_dalle):
+#         dalle.generate_and_download(transcript, "./imgs/tmp")
+#         temp_dir = "./imgs/tmp"
+#         targ_dir = "./imgs"
+#         i = 0
+#         for file_name in os.listdir(temp_dir):
+#             os.rename(os.path.join(temp_dir,file_name), os.path.join(targ_dir,transcript + "-" + str(i) + ".webp"))
+#             i += 1
 
-        display_image(display, os.path.join(targ_dir, transcript + "-0" + ".webp"), transcript)
-    else:
-        print("No Dalle Test")
+#         display_image(display, os.path.join(targ_dir, transcript + "-0" + ".webp"), transcript)
+#     else:
+#         print("No Dalle Test")
         
-        targ_dir = "./imgs"
-        file_name = "Show me a man walking a porcupine with a hat on.-1.webp"
-        transcript = "Show me a man walking a porcupine with a hat on."
+#         targ_dir = "./imgs"
+#         file_name = "Show me a man walking a porcupine with a hat on.-1.webp"
+#         transcript = "Show me a man walking a porcupine with a hat on."
 
-        display_image(display, os.path.join(targ_dir, file_name), transcript)
+#         display_image(display, os.path.join(targ_dir, file_name), transcript)
 
     
-    run_display()
+#     run_display()
 
-run_display()
+# run_display()
+
+
+# Create porcupine voice manager
+porcupine = pvporcupine.create(
+                access_key=pico_key,
+                keywords=["jarvis"])
+
+# Create Audio Stream
+input = pyaudio.PyAudio()
+FRAMES_PER_BUFFER = 3200
+FRAME_LENGTH = 512 # (TODO)
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+stream = input.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=porcupine.sample_rate,
+            input_device_index=-1,
+            input=True,
+            frames_per_buffer=porcupine.frame_length
+        )
+
+# Create event Loop
+loop = asyncio.get_event_loop()
+
+# Create WakeWord Event
+wakeword_event = asyncio.Event()
+
+async def voice_trigger_setter():
+    print("set")
+    try:
+        while True:
+            pcm = stream.read(porcupine.frame_length)
+            pcm_tuple = struct.unpack_from("h" * porcupine.frame_length, pcm)
+            result = porcupine.process(pcm_tuple)
+
+            if result >= 0:                    
+                wakeword_event.set()
+                break
+    except Exception as e:
+        print("voice_trigger_setter Exception")
+        print(e)
+
+async def voice_trigger_waiter():
+    try:
+        await wakeword_event.wait()
+    except Exception as e:
+        print("voice_trigger_waiter Exception")
+        print(e)
+
+
+async def connect_assembly_ai():
+    print("connected")
+
+async def display_thinking():
+    await display_image(display, "./imgs/thinking.webp", "Let me get ready...")
+
+URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
+
+async def send_receive():
+   print(f'Connecting websocket to url ${URL}')
+   async with websockets.connect(
+       URL,
+       extra_headers=(("Authorization", auth_key),),
+       ping_interval=5,
+       ping_timeout=20
+   ) as _ws:
+       await asyncio.sleep(0.1)
+       print("Receiving SessionBegins ...")
+       session_begins = await _ws.recv()
+       print(session_begins)
+       print("Sending messages ...")
+       async def send():
+           while True:
+               try:
+                   data = stream.read(FRAMES_PER_BUFFER)
+                   data = base64.b64encode(data).decode("utf-8")
+                   json_data = json.dumps({"audio_data":str(data)})
+                   await _ws.send(json_data)
+               except websockets.exceptions.ConnectionClosedError as e:
+                   print(e)
+                   assert e.code == 4008
+                   break
+               except Exception as e:
+                   assert False, "Not a websocket 4008 error"
+               await asyncio.sleep(0.01)
+          
+           return True
+      
+       async def receive():
+           while True:
+               try:
+                   result_str = await _ws.recv()
+                   print(json.loads(result_str)['text'])
+               except websockets.exceptions.ConnectionClosedError as e:
+                   print(e)
+                   assert e.code == 4008
+                   break
+               except Exception as e:
+                   assert False, "Not a websocket 4008 error"
+      
+       send_result, receive_result = await asyncio.gather(send(), receive())
+
+async def main():
+    print("Hello, World")
+    await asyncio.gather(voice_trigger_setter(), voice_trigger_waiter())
+    print("Awake")
+    await asyncio.gather(display_thinking(), send_receive())
+    print("done")
+
+asyncio.run(main())
