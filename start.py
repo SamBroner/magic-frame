68ca983f957cccbba507ad39b4bf6aafdf5d8532
Sam Broner <sam.broner@gmail.com>
2022-12-06 23:26:40 +0000

    Add an additional thread for audio capture. Add frame to transport audio from thread to thread Add trigger_frames to prepend the trigger word audio onto the prompt

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
-import glob,os
+import glob
+import os
 from dotenv import load_dotenv
 import logging
 import time
@@ -7,9 +8,10 @@ import urllib
 import urllib.request
 from pathlib import Path
 import asyncio
+import threading
 
 import pvporcupine
-import pvcheetah
+from collections import deque
 
 import pyaudio
 import websockets
@@ -38,7 +40,8 @@ pico_key = os.getenv('PICO_KEY')
 image_path = os.getenv('IMAGE_PATH')
 assembly_key = os.getenv('ASSEMBLY_KEY')
 
-display = AutoEPDDisplay(vcom=-2.06, rotate='CW', mirror=False, spi_hz=24000000)
+display = AutoEPDDisplay(vcom=-2.06, rotate='CW',
+                         mirror=False, spi_hz=24000000)
 
 default_display(display)
 
@@ -69,39 +72,48 @@ test_dalle = False
 #         display_image(display, os.path.join(targ_dir, transcript + "-0" + ".webp"), transcript)
 #     else:
 #         print("No Dalle Test")
-        
+
 #         targ_dir = "./imgs"
 #         file_name = "Show me a man walking a porcupine with a hat on.-1.webp"
 #         transcript = "Show me a man walking a porcupine with a hat on."
 
 #         display_image(display, os.path.join(targ_dir, file_name), transcript)
 
-    
+
 #     run_display()
 
 # run_display()
 
-
-# Create porcupine voice manager
-porcupine = pvporcupine.create(
-                access_key=pico_key,
-                keywords=["jarvis"])
-
 # Create Audio Stream
 input = pyaudio.PyAudio()
 FRAMES_PER_BUFFER = 3200
-FRAME_LENGTH = 512 # (TODO)
+FRAME_LENGTH = 512  # (TODO)
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
+
+# Create porcupine voice manager
+porcupine = pvporcupine.create(
+    access_key=pico_key,
+    keywords=["jarvis"])
+
 stream = input.open(
-            format=FORMAT,
-            channels=CHANNELS,
-            rate=porcupine.sample_rate,
-            input_device_index=-1,
-            input=True,
-            frames_per_buffer=porcupine.frame_length
-        )
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=porcupine.sample_rate,
+    input_device_index=-1,
+    input=True,
+    frames_per_buffer=porcupine.frame_length)
+
+frames = deque(maxlen=1000)
+
+# Create Audio Capture Thread. The frames object is used to transport audio across threads
+def audio_capture():
+    while True:
+        pcm = stream.read(porcupine.frame_length)
+        frames.append(pcm)
+        time.sleep(0.01)
+
 
 # Create event Loop
 loop = asyncio.get_event_loop()
@@ -109,20 +121,31 @@ loop = asyncio.get_event_loop()
 # Create WakeWord Event
 wakeword_event = asyncio.Event()
 
+
 async def voice_trigger_setter():
     print("set")
+
+    # This frames object is used to re-add the audio of asking the question of Jarvis
+    trigger_frames = deque(maxlen=100)
     try:
         while True:
-            pcm = stream.read(porcupine.frame_length)
-            pcm_tuple = struct.unpack_from("h" * porcupine.frame_length, pcm)
-            result = porcupine.process(pcm_tuple)
+            if len(frames) > 0:
+                pcm = frames.popleft()
+                pcm_tuple = struct.unpack_from(
+                    "h" * porcupine.frame_length, pcm)
+                result = porcupine.process(pcm_tuple)
+                trigger_frames.append(pcm)
+                if result >= 0:
+                    wakeword_event.set()
+                    frames.clear()
+                    while (len(trigger_frames) > 0):
+                        frames.appendleft(trigger_frames.pop())
 
-            if result >= 0:                    
-                wakeword_event.set()
-                break
+                    break
     except Exception as e:
         print("voice_trigger_setter Exception")
         print(e)
+
 
 async def voice_trigger_waiter():
     try:
@@ -134,6 +157,7 @@ async def voice_trigger_waiter():
 
 async def connect_assembly_ai():
     print("connected")
+
 
 async def display_thinking():
     display_image(display, "./imgs/thinking.webp", "Let me get ready...")
@@ -153,13 +177,21 @@ async def send_receive():
         session_begins = await _ws.recv()
         print(session_begins)
         print("Sending messages ...")
+
         async def send():
             while True:
                 try:
-                    data = stream.read(FRAMES_PER_BUFFER)
-                    data = base64.b64encode(data).decode("utf-8")
-                    json_data = json.dumps({"audio_data":str(data)})
-                    await _ws.send(json_data)
+                    if (len(frames) > 6):
+                        one = bytes(frames.popleft())
+                        two = bytes(frames.popleft())
+                        three = bytes(frames.popleft())
+                        four = bytes(frames.popleft())
+                        five = bytes(frames.popleft())
+                        six = bytes(frames.popleft())
+                        data = b''.join([one, two, three, four, five, six])
+                        data = base64.b64encode(data).decode("utf-8")
+                        json_data = json.dumps({"audio_data": str(data)})
+                        await _ws.send(json_data)
                 except websockets.exceptions.ConnectionClosedError as e:
                     print(e)
                     assert e.code == 4008
@@ -167,18 +199,20 @@ async def send_receive():
                 except Exception as e:
                     print(e)
                     break
-                    # TODO: eat this error. It's because we close the session in receive and continue sending data.
+                    # TODO: We eat this error. It's because we close the session in receive and continue sending data.
                 await asyncio.sleep(0.01)
-            
+
             return True
-      
+
         async def receive():
+            print("hello")
             while True:
                 try:
+                    print("receiving...")
                     result_str = await _ws.recv()
                     load = json.loads(result_str)
                     print(load['text'])
-                    if(load["message_type"] == "FinalTranscript"):
+                    if (load["message_type"] == "FinalTranscript"):
                         json_data = json.dumps({"terminate_session": "true"})
                         await _ws.send(json_data)
                         await _ws.close()
@@ -189,18 +223,28 @@ async def send_receive():
                     break
                 except Exception as e:
                     assert False, "Not a websocket 4008 error"
-      
+
         send_result, receive_result = await asyncio.gather(send(), receive())
-        # return await asyncio.gather(send(), receive())
         return receive_result
-        
+
 
 async def main():
     print("Hello, World")
     await asyncio.gather(voice_trigger_setter(), voice_trigger_waiter())
     print("Awake")
-    _, text = await asyncio.gather(display_thinking(), send_receive())
+    _, prompt = await asyncio.gather(display_thinking(), send_receive())
+    print(prompt)
     print("done")
-    print(text)
+
+thread = threading.Thread(target=audio_capture)
+thread.start()
+
+# Wait for the thread to start running, with a timeout of 5 seconds
+try:
+    thread.join(timeout=5)
+except TimeoutError:
+    print("The thread did not start running within 5 seconds.")
+else:
+    print("The thread started running.")
 
 asyncio.run(main())
