f549abc032d94ed29cd49d9efd4938944c7a2807
Sam Broner <sam.broner@gmail.com>
2022-12-06 06:02:12 +0000

    getting good text data from assemblyai

--- start.py
+++ start.py
@@ -136,59 +136,77 @@ async def connect_assembly_ai():
     print("connected")
 
 async def display_thinking():
-    await display_image(display, "./imgs/thinking.webp", "Let me get ready...")
+    display_image(display, "./imgs/thinking.webp", "Let me get ready...")
 
 URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
 
 async def send_receive():
-   print(f'Connecting websocket to url ${URL}')
-   async with websockets.connect(
-       URL,
-       extra_headers=(("Authorization", auth_key),),
-       ping_interval=5,
-       ping_timeout=20
-   ) as _ws:
-       await asyncio.sleep(0.1)
-       print("Receiving SessionBegins ...")
-       session_begins = await _ws.recv()
-       print(session_begins)
-       print("Sending messages ...")
-       async def send():
-           while True:
-               try:
-                   data = stream.read(FRAMES_PER_BUFFER)
-                   data = base64.b64encode(data).decode("utf-8")
-                   json_data = json.dumps({"audio_data":str(data)})
-                   await _ws.send(json_data)
-               except websockets.exceptions.ConnectionClosedError as e:
-                   print(e)
-                   assert e.code == 4008
-                   break
-               except Exception as e:
-                   assert False, "Not a websocket 4008 error"
-               await asyncio.sleep(0.01)
-          
-           return True
+    print(f'Connecting websocket to url ${URL}')
+    async with websockets.connect(
+        URL,
+        extra_headers=(("Authorization", assembly_key),),
+        ping_interval=5,
+        ping_timeout=20
+    ) as _ws:
+        await asyncio.sleep(0.1)
+        print("Receiving SessionBegins ...")
+        session_begins = await _ws.recv()
+        print(session_begins)
+        print("Sending messages ...")
+        async def send():
+            while True:
+                try:
+                    data = stream.read(FRAMES_PER_BUFFER)
+                    data = base64.b64encode(data).decode("utf-8")
+                    json_data = json.dumps({"audio_data":str(data)})
+                    await _ws.send(json_data)
+                except websockets.exceptions.ConnectionClosedError as e:
+                    print("--A--")
+                    print(e)
+                    assert e.code == 4008
+                    break
+                except Exception as e:
+                    print("--B--")
+                    print(e)
+                    break
+                    # TODO: eat this error. It's because we close the session in receive and continue sending data.
+                await asyncio.sleep(0.01)
+            
+            return True
       
-       async def receive():
-           while True:
-               try:
-                   result_str = await _ws.recv()
-                   print(json.loads(result_str)['text'])
-               except websockets.exceptions.ConnectionClosedError as e:
-                   print(e)
-                   assert e.code == 4008
-                   break
-               except Exception as e:
-                   assert False, "Not a websocket 4008 error"
+        async def receive():
+            while True:
+                try:
+                    result_str = await _ws.recv()
+                    load = json.loads(result_str)
+                    print(load['text'])
+                    if(load["message_type"] == "FinalTranscript"):
+                        json_data = json.dumps({"terminate_session": "true"})
+                        await _ws.send(json_data)
+                        await _ws.close()
+                        return load['text']
+                except websockets.exceptions.ConnectionClosedError as e:
+                    print("--C--")
+
+                    print(e)
+                    assert e.code == 4008
+                    break
+                except Exception as e:
+                    print("--D--")
+
+                    assert False, "Not a websocket 4008 error"
       
-       send_result, receive_result = await asyncio.gather(send(), receive())
+        send_result, receive_result = await asyncio.gather(send(), receive())
+        # return await asyncio.gather(send(), receive())
+        return receive_result
+        
 
 async def main():
     print("Hello, World")
     await asyncio.gather(voice_trigger_setter(), voice_trigger_waiter())
     print("Awake")
-    await asyncio.gather(display_thinking(), send_receive())
+    _, text = await asyncio.gather(display_thinking(), send_receive())
     print("done")
+    print(text)
 
 asyncio.run(main())
